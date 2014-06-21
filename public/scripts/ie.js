25c1904d017c4036cf01b958e83a45bb09dfabfd
Allison Lewis <allison@armchairmedia.com>
2014-06-21 12:30:29 -0400

    Styled widget for IE 8, also centered container div, added gray background to body (temporary) to show widget, converted to HTML

--- public/scripts/ie.js
+++ public/scripts/ie.js
@@ -9,11 +9,8 @@ $(document).ready(function() {
         head.removeChild(style);
     }, 0);
 
-
-    $('body').addClass('oldIE');
-
-
-    // $(".kroger-rsu-modal h3.rsu-btn").corner("12px");
+    $("#top5-list > li.top5 > a > div.icon-container").corner("36px");
+    $("#top5-list > li.top5 > section.pContainer").corner("12px");
     // $(".kroger-rsu-modal h3.rsu-btn span.rsu-icon").corner("24px");
     // $(".kroger-rsu-modal .rsu-container").corner("12px");
     // $(".kroger-rsu-modal .rsu-container a.seeRewardsStatement").corner("12px");
