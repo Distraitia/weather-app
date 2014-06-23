fbdfcdc992662e22abca949c4b97a3e2df808d76
Allison Lewis <allison@armchairmedia.com>
2014-06-23 12:08:07 -0400

    Adjusted media queries so mobile view toggles below 600px instead of 744px, added close button, converted to HTML

--- converted-html/public/scripts/main.js
+++ converted-html/public/scripts/main.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
 	$('#top5-list').children('li').first().children('a').addClass('active').next().addClass('is-open').show();
     
-	$('#top5-list').on('click', 'li > a', function(e) {
+	$('#top5-list').on('click', 'li.top5 > a', function(e) {
 		e.preventDefault();
     
 	  	if (!$(this).hasClass('active')) {
@@ -16,6 +16,29 @@ $(document).ready(function() {
 	    	$('#top5-list .is-open').removeClass('is-open').hide();
 	    	$(this).removeClass('active');
 	  	}
+	});
+
+	$(".top5-why .block1 ol li ul li:first-child").on("click", function() {
+		if ($(".top5-when > a").hasClass("active")) {
+			// do nothing
+		} else {
+			$('#top5-list .is-open').removeClass('is-open').hide();
+		    $(".top5-when > a").next().toggleClass('is-open').toggle();
+		      
+		    $('#top5-list').find('.active').removeClass('active');
+		    $(".top5-when > a").addClass('active');
+		}
+	});
+	$(".top5-why .block1 ol li ul li:last-child").on("click", function() {
+		if ($(".top5-real-money > a").hasClass("active")) {
+			// do nothing
+		} else {
+			$('#top5-list .is-open').removeClass('is-open').hide();
+		    $(".top5-real-money > a").next().toggleClass('is-open').toggle();
+		      
+		    $('#top5-list').find('.active').removeClass('active');
+		    $(".top5-real-money > a").addClass('active');
+		}
 	});
 
 });
