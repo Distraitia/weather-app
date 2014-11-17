f757b8be2ce9fa7b4759f3b6b86f02559fb5707f
Allison Lewis <allison@armchairmedia.com>
2014-11-16 19:04:36 -0500

    Browser-tested for IE8+, finished styling, still need to build subpage

--- assets/source-js/main.js
+++ assets/source-js/main.js
@@ -1,44 +1,46 @@
 $(document).ready(function() {
 
-	$('#top5-list').children('li').first().children('a').addClass('active').next().addClass('is-open').show();
-    
-	$('#top5-list').on('click', 'li.top5 > a', function(e) {
-		e.preventDefault();
-    
-	  	if (!$(this).hasClass('active')) {
-
-		    $('#top5-list .is-open').removeClass('is-open').hide();
-		    $(this).next().toggleClass('is-open').toggle();
-		      
-		    $('#top5-list').find('.active').removeClass('active');
-		    $(this).addClass('active');
-	  	} else {
-	    	$('#top5-list .is-open').removeClass('is-open').hide();
-	    	$(this).removeClass('active');
-	  	}
-	});
-
-	$(".top5-why .block1 ol li ul li:first-child").on("click", function() {
-		if ($(".top5-when > a").hasClass("active")) {
-			// do nothing
-		} else {
-			$('#top5-list .is-open').removeClass('is-open').hide();
-		    $(".top5-when > a").next().toggleClass('is-open').toggle();
-		      
-		    $('#top5-list').find('.active').removeClass('active');
-		    $(".top5-when > a").addClass('active');
-		}
-	});
-	$(".top5-why .block1 ol li ul li:last-child").on("click", function() {
-		if ($(".top5-real-money > a").hasClass("active")) {
-			// do nothing
-		} else {
-			$('#top5-list .is-open').removeClass('is-open').hide();
-		    $(".top5-real-money > a").next().toggleClass('is-open').toggle();
-		      
-		    $('#top5-list').find('.active').removeClass('active');
-		    $(".top5-real-money > a").addClass('active');
-		}
-	});
+	// if IE 8, get the size of the window and assign size classes to container of the grid blocks
+    if ($("html").hasClass("ie8")) {
+        function removeIE8SizeClasses() {
+            $("body").removeClass("size-mobile").removeClass("size-tablet").removeClass("size-tablet-A").removeClass("size-tablet-B").removeClass("size-desktop").removeClass("size-desktop-A").removeClass("size-desktop-B");
+                clearIE8Borders();
+        }
+        function clearIE8Borders() {
+            $(".grid-block").removeClass("rightColEnd").removeClass("topBorder");
+        }
+        function setWindowSizes() {
+            var windowSize = $(window).width();
+            if (windowSize <= 480) {
+                removeIE8SizeClasses();
+                $("body").addClass("size-mobile");
+            } else if (windowSize > 480 && windowSize <= 1024) {
+                if (windowSize <= 767) {
+                    removeIE8SizeClasses();
+                    $("body").addClass("size-tablet size-tablet-A");
+                } else {
+                    removeIE8SizeClasses();
+                    $("body").addClass("size-tablet size-tablet-B");
+                }
+                $(".ie8 .size-tablet .grid-block:not(.new-icon-block):nth-child(even)").addClass("rightColEnd");
+                $(".ie8 .size-tablet .grid-block:not(.new-icon-block):nth-child(-n+2)").addClass("topBorder");
+                $(".new-icon-block:nth-child(5n-4)").addClass("leftColEnd");
+            } else if (windowSize > 1024) {
+                if (windowSize <= 1199) {
+                    removeIE8SizeClasses();
+                    $("body").addClass("size-desktop size-desktop-A");
+                } else {
+                    removeIE8SizeClasses();
+                    $("body").addClass("size-desktop size-desktop-B");
+                }
+                $(".ie8 .size-desktop .grid-block:not(.new-icon-block):nth-child(3n)").addClass("rightColEnd");
+                $(".ie8 .size-desktop .grid-block:not(.new-icon-block):nth-child(-n+3)").addClass("topBorder");
+                $(".ie8 .size-desktop .new-icon-block:nth-child(4n)").addClass("rightColEnd");
+                $(".ie8 .size-desktop .new-icon-block:nth-child(-n+4)").addClass("topBorder");
+            }
+        }
+        setWindowSizes();
+        $(window).on("resize", setWindowSizes);
+    }
 
 });
