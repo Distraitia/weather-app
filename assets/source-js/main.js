765760a6f3bdfffacfbdaf6d9e45b91f970b612e
Allison Lewis <allison@armchairmedia.com>
2014-11-16 21:53:45 -0500

    Deleted old modal files from other project, finished browser-testing for IE8+, styled for all viewport sizes

--- assets/source-js/main.js
+++ assets/source-js/main.js
@@ -1,3 +1,39 @@
+  // Smart resize avoids polling resize event too much
+  (function($,sr){
+
+    // debouncing function from John Hann
+    // http://unscriptable.com/index.php/2009/03/20/debouncing-javascript-methods/
+    var debounce = function (func, threshold, execAsap) {
+      var timeout;
+
+      return function debounced () {
+        var obj = this, args = arguments;
+        function delayed () {
+          if (!execAsap)
+            func.apply(obj, args);
+          timeout = null; 
+        };
+
+        if (timeout)
+          clearTimeout(timeout);
+        else if (execAsap)
+          func.apply(obj, args);
+
+        timeout = setTimeout(delayed, threshold || 100); 
+      };
+    }
+    // smartresize 
+    $.fn[sr] = function(fn){  return fn ? this.bind('resize', debounce(fn)) : this.trigger(sr); };
+
+    // USE IT LIKE THIS
+    /* If the window is resized, we better resize the images to fit.
+    $(window).smartresize(function(){  
+        // code that takes it easy...   
+    });
+  */
+
+})($,'smartresize');
+
 $(document).ready(function() {
 
 	// if IE 8, get the size of the window and assign size classes to container of the grid blocks
@@ -40,7 +76,8 @@ $(document).ready(function() {
             }
         }
         setWindowSizes();
-        $(window).on("resize", setWindowSizes);
+        // $(window).on("resize", setWindowSizes);
+        $(window).smartresize(setWindowSizes);
     }
 
 });
